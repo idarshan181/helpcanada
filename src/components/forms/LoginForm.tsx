6412d6e1021d683639ad2c5299fed3cbbcf683d1
Darshan Patel <ddpatel97@gmail.com>
2025-03-29 11:09:53 -0230

    feat: added confetti

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -33,7 +33,7 @@ export default async function LoginForm() {
                 'use server';
 
                 await signIn('google', {
-                  redirectTo: '/dashboard',
+                  redirectTo: '/',
                 });
               }}
             >
